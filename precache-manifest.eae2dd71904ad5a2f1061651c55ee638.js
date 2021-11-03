0e2680757f84d4eede29917e2c657319c5ef9c54
“Alex <alexandriadelaney923@gmail.com>
2021-11-03 13:24:22 -0700

    Updates

---
+++ precache-manifest.eae2dd71904ad5a2f1061651c55ee638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9189f71d6fa9c00801445d75781c94",
+    "url": "/easy-vegan/index.html"
+  },
+  {
+    "revision": "a86d016bc8904fdc0cde",
+    "url": "/easy-vegan/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "0634bffd8dcc28860d2f",
+    "url": "/easy-vegan/static/css/main.12222a4d.chunk.css"
+  },
+  {
+    "revision": "a86d016bc8904fdc0cde",
+    "url": "/easy-vegan/static/js/2.7179a28d.chunk.js"
+  },
+  {
+    "revision": "0634bffd8dcc28860d2f",
+    "url": "/easy-vegan/static/js/main.cbcf6152.chunk.js"
+  },
+  {
+    "revision": "5e18f91f7707830df010",
+    "url": "/easy-vegan/static/js/runtime-main.42611dca.js"
+  }
+]);
